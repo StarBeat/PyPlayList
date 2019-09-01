d3141ded7e9afe306a06f2552b58506e066b31d7
StarBeat <977663818@qq.com>
2019-09-01 22:48:53 +0800

    create repo

---
+++ Control.py
@@ -0,0 +1,72 @@
+from datetime import timedelta
+from abc import ABC, abstractmethod
+import Models
+
+class IPlayListWR(object):
+    @abstractmethod
+    def PaserFromStr(self, arg:str):
+        pass
+
+    @abstractmethod
+    def ToText(self, arg:Models.IBaseList):
+        pass
+
+class M3u8Content(IPlayListWR):
+    def PaserFromStr(self, arg:str):
+        m3 = Models.M3u()
+        lins :list = arg.splitlines()
+        if lins[0] == "#EXTM3U":
+            m3.is_extened = True
+        else:
+            m3.is_extened = False
+            m3.entities.append(Models.M3u.M3uBase(title = "", duration = 0))
+            m3.path = lins[0]
+        prevLineIsExtInf :bool = False
+        title :str = ""
+        artist :str = ""
+        album :str = ""
+        seconds :int = 0
+        for i in lins:
+            if i.startwith("#"):
+                if m3.is_extened:
+                    if i.startwith("#EXTINF"):
+                        prevLineIsExtInf = True
+                        title = i[i.index(',') + 1,]
+                        seconds = int(i[8, i.index(',') - 8])
+                    elif i.startwith("#EXTALB"):
+                        album = i[i.index(":") + 1,]
+            else:
+                if not m3.is_extened or not prevLineIsExtInf:
+                    title = ""
+                    artist = ""
+                    album = ""
+                    seconds = 0
+                m3.entities.append(Models.M3u.M3uBase(album = album, album_artist = artist, duration = timedelta(seconds), title = title))
+                m3.path = i
+                prevLineIsExtInf = False
+        return m3
+
+    def ToText(self, args):
+        m3 = Models.M3u(args)
+        text :str = ""#EXTM3U"\n"
+        for x in m3.entities:
+            if m3.is_extened:
+                if x.album != None:
+                    text += "#EXTALB:" + x.album
+                if x.album_artist != None:
+                    text += "#EXTART:" + x.album_artist
+                text += "#EXTINF:" + timedelta(x.duration).seconds + "," + x.title
+            text += m3.path
+        return text
+
+class M3uContent(object):
+    pass
+
+class PlsContent(object):
+    pass
+
+class WplContent(object):
+    pass
+
+class ZplContent(object):
+    pass
---
+++ Models.py
@@ -0,0 +1,78 @@
+from datetime import timedelta
+
+class IBaseList(object):
+    def __init__(self, path :str = None, file_name :str  = None, entities :list  = None):
+        self.path :str = path
+        self.file_name :str = file_name
+        self.entities :list = entities
+    class Entity():
+        def __init(self, path :str = None):
+            self.path :str = path
+
+    def GetTracksPaths(self):
+        paths : list = None
+        for i in self.entities:
+            paths.append(self.Entity(i).path)
+        return paths
+
+class M3u(IBaseList):
+    def __init__(self, is_extened :bool = False):
+        self.is_extened :bool = is_extened
+    is_extened :bool
+    class M3uBase:
+        def __init__(self, title :str = None, album :str = None, album_artist :str = None, duration :timedelta = None):
+            self.title :str = title
+            self.album :str = album
+            self.album_artist :str = album_artist
+            self.duration :timedelta = duration
+
+class Pls(IBaseList):
+    def __init__(self, version :int = None):
+        self.version :int = 2
+    class  PlsBase:
+        def __init(self, title :str = None, length :timedelta = None, nr :int = None):
+            self.title :str
+            self.length :timedelta
+            self.nr :int
+
+    def GetNums(self):
+        return len(self.entities)
+
+class Wpl(IBaseList):
+    def __init__(self, author :str = None, generator :str = None, 
+                 guid :str = None, item_count :int = None, title :str = None, total_duration:timedelta = None):
+        self.author :str = author
+        self.generator :str =generator
+        self.guid :str = guid
+        self.item_count :int = item_count
+        self.title :str = title
+        self.total_duration:timedelta =total_duration
+
+    class WplBase:
+        def __init__(self, album_title :str = None, album_artist :str = None, 
+                     duration :timedelta = None, track_title :str = None, track_artist :str = None):
+            self.album_title :str = album_title
+            self.album_artist :str = album_artist
+            self.duration :timedelta = duration
+            self.track_title :str = track_title
+            self.track_artist :str = track_artist
+
+
+class Zpl(IBaseList):
+    def __init__(self, author :str = None, generator :str = None, 
+                 guid :str = None, item_count :int = None, title :str = None, total_duration:timedelta = None):
+        self.author :str = author
+        self.generator :str =generator
+        self.guid :str = guid
+        self.item_count :int = item_count
+        self.title :str = title
+        self.total_duration:timedelta =total_duration
+
+    class ZplBase:
+        def __init__(self, album_title :str = None, album_artist :str = None, 
+                     duration :timedelta = None, track_title :str = None, track_artist :str = None):
+            self.album_title :str = album_title
+            self.album_artist :str = album_artist
+            self.duration :timedelta = duration
+            self.track_title :str = track_title
+            self.track_artist :str = track_artist
